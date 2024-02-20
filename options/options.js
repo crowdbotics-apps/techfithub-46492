cda3a62ab0374f12643298d2312c36304e327015
Crowdbotics <team@crowdbotics.com>
2024-02-20 12:48:57 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "techfithub-46492",
+  url: "https://techfithub-46492.botics.co",
+  api: "https://techfithub-46492.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
